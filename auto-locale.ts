ea26874ae3dfe4168841ffea2a885492f5f5201e
Alessio Pareto (Desktop-Pare) <alessio.pareto@nixpare.com>
2025-06-23 19:13:57 +0200

    Added useT hook and improved swap

--- auto-locale.ts
+++ auto-locale.ts
@@ -11,21 +11,31 @@ export interface AutoLocaleOptions {
 	exclude?: string | string[];
 	// a list of languages to parse
 	locales: string[];
+	// default locale
+	default: string;
 	// default 'T'
-	helperName?: string;
+	componentHelperName?: string;
+	// default 'useT'
+	hookHelperName?: string;
 	// default 'useLocale'
 	useLocaleName?: string;
+	// path to import useLocale implementation
 	useLocaleImportPath: string;
 }
 
 export default function autoLocalePlugin(options: AutoLocaleOptions): Plugin {
 	const filter = createFilter(options.include, options.exclude);
 	const locales = options.locales;
-	const helper = options.helperName || 'T';
+	const defaultLocale = options.default;
+	const componentHelper = options.componentHelperName || 'T';
+	const hookHelper = options.hookHelperName || 'useT';
 	const useLocaleName = options.useLocaleName || 'useLocale';
 	const useLocaleImportPath = options.useLocaleImportPath;
 
 	const componentPrefix = "AutoLocale"
+	const hookPrefix = "useAutoLocale"
+	const reactImportAlias = t.identifier("AutoLocale_React")
+	const useLocaleImportAlias = t.identifier("AutoLocale_useLocale")
 
 	const virtualModules: Record<string, string> = {};
 	const virtualModulePrefix = 'virtual:auto-locale'
@@ -69,7 +79,7 @@ export default function autoLocalePlugin(options: AutoLocaleOptions): Plugin {
 			// @ts-ignore
 			(traverse.default as typeof traverse)(ast, {
 				JSXElement(nodePath) {
-					if (!t.isJSXIdentifier(nodePath.node.openingElement.name, { name: helper })) {
+					if (!t.isJSXIdentifier(nodePath.node.openingElement.name, { name: componentHelper })) {
 						return;
 					}
 
@@ -88,7 +98,6 @@ export default function autoLocalePlugin(options: AutoLocaleOptions): Plugin {
 						}
 					});
 
-					// Extract each locale map
 					localeAttrs.forEach(attr => {
 						if (t.isJSXIdentifier(attr.name)) {
 							if (t.isStringLiteral(attr.value) || t.isJSXElement(attr.value) || t.isJSXFragment(attr.value)) {
@@ -101,11 +110,12 @@ export default function autoLocalePlugin(options: AutoLocaleOptions): Plugin {
 						}
 					});
 
-					// Replace call with React.lazy import
+					const virtualModuleId = (locale: string) => t.stringLiteral(`${virtualModulePrefix}/${locale}.jsx`)
+
 					const importCall = (locale: string) => {
 						const importDecl = t.callExpression(
 							t.import(),
-							[t.stringLiteral(`${virtualModulePrefix}/${locale}.jsx`)]
+							[virtualModuleId(locale)]
 						)
 
 						const importWithThenDecl = t.callExpression(
@@ -130,7 +140,7 @@ export default function autoLocalePlugin(options: AutoLocaleOptions): Plugin {
 						);
 
 						return t.callExpression(
-							t.memberExpression(t.identifier('React'), t.identifier('lazy')),
+							t.memberExpression(reactImportAlias, t.identifier('lazy')),
 							[t.arrowFunctionExpression(
 								[],
 								importWithThenDecl
@@ -140,44 +150,17 @@ export default function autoLocalePlugin(options: AutoLocaleOptions): Plugin {
 
 					const compName = `${componentPrefix}_${key}`
 
-					const lazyCompDecls = locales.map(locale => t.variableDeclaration(
-						"const",
-						[t.variableDeclarator(
-							t.identifier(`${compName}_${locale}`),
-							importCall(locale)
-						)]
-					))
+					const lazyCompDecls = locales.map(locale => {
+						const compId = t.identifier(`${compName}_${locale}`)
 
-					const localeId = t.identifier('locale');
-					const useLocaleDecl = t.variableDeclaration("const", [
-						t.variableDeclarator(
-							localeId,
-							t.callExpression(t.identifier(useLocaleName), [])
+						return t.variableDeclaration(
+							"const",
+							[t.variableDeclarator(
+								compId,
+								importCall(locale)
+							)]
 						)
-					])
-
-					const prevLocaleId = t.identifier('prevLocaleRef')
-					const usePrevLocaleDecl = t.variableDeclaration('const', [t.variableDeclarator(
-						prevLocaleId,
-						t.callExpression(t.memberExpression(t.identifier('React'), t.identifier('useRef')), [t.identifier('locale')])
-					)])
-
-					const useEffectUpdatePrevDecl = t.expressionStatement(
-						t.callExpression(t.memberExpression(t.identifier('React'), t.identifier('useEffect')), [
-							t.arrowFunctionExpression(
-								[],
-								t.blockStatement([
-									t.expressionStatement(
-										t.assignmentExpression('=',
-											t.memberExpression(t.identifier('prevLocaleRef'), t.identifier('current')),
-											t.identifier('locale')
-										)
-									)
-								])
-							),
-							t.arrayExpression([t.identifier('locale')])
-						])
-					)
+					})
 
 					const mapId = t.identifier('Map')
 					const mapDecl = t.variableDeclaration('const', [
@@ -194,6 +177,37 @@ export default function autoLocalePlugin(options: AutoLocaleOptions): Plugin {
 						)
 					]);
 
+					const localeId = t.identifier('locale');
+					const useLocaleDecl = t.variableDeclaration("const", [
+						t.variableDeclarator(
+							localeId,
+							t.callExpression(useLocaleImportAlias, [])
+						)
+					])
+
+					const prevLocaleId = t.identifier('prevLocaleRef')
+					const usePrevLocaleDecl = t.variableDeclaration('const', [t.variableDeclarator(
+						prevLocaleId,
+						t.callExpression(t.memberExpression(reactImportAlias, t.identifier('useRef')), [t.identifier('locale')])
+					)])
+
+					const useEffectUpdatePrevDecl = t.expressionStatement(
+						t.callExpression(t.memberExpression(reactImportAlias, t.identifier('useEffect')), [
+							t.arrowFunctionExpression(
+								[],
+								t.blockStatement([
+									t.expressionStatement(
+										t.assignmentExpression('=',
+											t.memberExpression(t.identifier('prevLocaleRef'), t.identifier('current')),
+											t.identifier('locale')
+										)
+									)
+								])
+							),
+							t.arrayExpression([t.identifier('locale')])
+						])
+					)
+
 					const selectedDecl = t.variableDeclaration('const', [
 						t.variableDeclarator(
 							t.identifier('Selected'),
@@ -208,18 +222,10 @@ export default function autoLocalePlugin(options: AutoLocaleOptions): Plugin {
 					const fallbackName = 'Fallback'
 					const fallbackDecl = t.variableDeclaration('const', [t.variableDeclarator(
 						t.identifier(fallbackName),
-						t.conditionalExpression(
-							t.binaryExpression(
-								"!==",
-								localeId,
-								t.memberExpression(prevLocaleId, t.identifier('current'))
-							),
-							t.memberExpression(
-								t.identifier('Map'),
-								t.memberExpression(t.identifier('prevLocaleRef'), t.identifier('current')),
-								true
-							),
-							t.nullLiteral()
+						t.memberExpression(
+							t.identifier('Map'),
+							t.memberExpression(t.identifier('prevLocaleRef'), t.identifier('current')),
+							true
 						)
 					)])
 
@@ -228,27 +234,23 @@ export default function autoLocalePlugin(options: AutoLocaleOptions): Plugin {
 					const returnStmt = t.returnStatement(
 						t.jsxElement(
 							t.jsxOpeningElement(
-								t.jsxMemberExpression(t.jsxIdentifier('React'), t.jsxIdentifier('Suspense')),
+								t.jsxMemberExpression(t.jsxIdentifier(reactImportAlias.name), t.jsxIdentifier('Suspense')),
 								[t.jsxAttribute(t.jsxIdentifier('fallback'), t.jsxExpressionContainer(
-									t.logicalExpression(
-										"&&",
-										t.identifier(fallbackName),
-										t.jsxElement(
-											t.jsxOpeningElement(
-												t.jsxIdentifier(fallbackName),
-												[ t.jsxSpreadAttribute(propsId) ],
-												true
-											),
-											null,
-											[],
+									t.jsxElement(
+										t.jsxOpeningElement(
+											t.jsxIdentifier(fallbackName),
+											[t.jsxSpreadAttribute(propsId)],
 											true
-										)
+										),
+										null,
+										[],
+										true
 									)
 								))],
 								false
 							),
 							t.jsxClosingElement(
-								t.jsxMemberExpression(t.jsxIdentifier('React'), t.jsxIdentifier('Suspense'))
+								t.jsxMemberExpression(t.jsxIdentifier(reactImportAlias.name), t.jsxIdentifier('Suspense'))
 							),
 							[
 								t.jsxElement(
@@ -269,10 +271,10 @@ export default function autoLocalePlugin(options: AutoLocaleOptions): Plugin {
 						t.identifier(compName),
 						[ propsId ],
 						t.blockStatement([
+							mapDecl,
 							useLocaleDecl,
 							usePrevLocaleDecl,
 							useEffectUpdatePrevDecl,
-							mapDecl,
 							selectedDecl,
 							fallbackDecl,
 							returnStmt
@@ -296,19 +298,240 @@ export default function autoLocalePlugin(options: AutoLocaleOptions): Plugin {
 					);
 					modified = true;
 				},
+				CallExpression(nodePath) {
+					if (!t.isIdentifier(nodePath.node.callee, { name: hookHelper })) {
+						return;
+					}
+
+					if (!t.isObjectExpression(nodePath.node.arguments[0])) {
+						return;
+					}
+
+					const baseName = path.basename(id, '.tsx');
+					const key = `${baseName}__${translationCount++}`;
+
+					const argAttr = nodePath.node.arguments[1] as typeof nodePath.node.arguments[1] | undefined;
+
+					nodePath.node.arguments[0].properties.forEach(prop => {
+						if (!t.isObjectProperty(prop) || !t.isIdentifier(prop.key)) {
+							return;
+						}
+
+						if (!locales.includes(prop.key.name)) {
+							return;
+						}
+
+						if (t.isExpression(prop.value)) {
+							translations.get(prop.key.name)!.set(key, prop.value);
+						} else {
+							console.error('invalid', prop.value);
+						}
+					});
+
+					const virtualModuleId = (locale: string) => t.stringLiteral(`${virtualModulePrefix}/${locale}.jsx`)
+					
+					const importCall = (locale: string) => {
+						const importDecl = t.callExpression(
+							t.import(),
+							[virtualModuleId(locale)]
+						)
+
+						const importWithThenDecl = t.callExpression(
+							t.memberExpression(importDecl, t.identifier('then')),
+							[
+								t.arrowFunctionExpression(
+									[t.identifier('module')],
+									t.memberExpression(
+										t.identifier('module'),
+										t.stringLiteral(key),
+										true
+									)
+								)
+							]
+						);
+
+						return t.arrowFunctionExpression(
+							[],
+							importWithThenDecl
+						);
+					}
+
+					const hookName = `${hookPrefix}_${key}`;
+
+					const defaultHookId = t.identifier(`${hookName}_default`);
+					const defaultHookDecl = t.importDeclaration(
+						[t.importSpecifier(defaultHookId, t.identifier(key))],
+						virtualModuleId(defaultLocale)
+					)
+
+					const asyncHooksDecls = locales.map(locale => {
+						if (locale === defaultLocale) {
+							return t.variableDeclaration(
+								"const",
+								[t.variableDeclarator(
+									t.identifier(`${hookName}_${locale}`),
+									t.arrowFunctionExpression(
+										[],
+										t.callExpression(
+											t.memberExpression(t.identifier('Promise'), t.identifier('resolve')),
+											[defaultHookId]
+										)
+									)
+								)]
+							)
+						} else {
+							return t.variableDeclaration(
+								"const",
+								[t.variableDeclarator(
+									t.identifier(`${hookName}_${locale}`),
+									importCall(locale)
+								)]
+							)
+						}
+					})
+
+					const mapId = t.identifier('map')
+					const mapDecl = t.variableDeclaration('const', [
+						t.variableDeclarator(
+							mapId,
+							t.objectExpression(
+								locales.map(locale =>
+									t.objectProperty(
+										t.identifier(locale),
+										t.identifier(`${hookName}_${locale}`)
+									)
+								)
+							)
+						)
+					]);
+
+					const localeId = t.identifier('locale');
+					const useLocaleDecl = t.variableDeclaration("const", [
+						t.variableDeclarator(
+							localeId,
+							t.callExpression(useLocaleImportAlias, [])
+						)
+					])
+
+					const prevLocaleId = t.identifier('prevLocaleRef')
+					const usePrevLocaleDecl = t.variableDeclaration('const', [t.variableDeclarator(
+						prevLocaleId,
+						t.callExpression(t.memberExpression(reactImportAlias, t.identifier('useRef')), [t.identifier('locale')])
+					)])
+
+					const propsId = t.identifier('props');
+
+					const resultId = t.identifier('result')
+					const setResultId = t.identifier('setResult')
+					const useStateResultDecl = t.variableDeclaration('const', [t.variableDeclarator(
+						t.arrayPattern([ resultId, setResultId ]),
+						t.callExpression(
+							t.memberExpression(reactImportAlias, t.identifier('useState')),
+							[t.callExpression(
+								defaultHookId,
+								[propsId]
+							)]
+						)
+					)])
+
+					const useEffectUpdatePrevDecl = t.expressionStatement(
+						t.callExpression(t.memberExpression(reactImportAlias, t.identifier('useEffect')), [
+							t.arrowFunctionExpression(
+								[],
+								t.blockStatement([
+									t.expressionStatement(
+										t.assignmentExpression('=',
+											t.memberExpression(t.identifier('prevLocaleRef'), t.identifier('current')),
+											t.identifier('locale')
+										)
+									)
+								])
+							),
+							t.arrayExpression([t.identifier('locale')])
+						])
+					)
+
+					const selectedId = t.identifier('selected')
+					const selectedDecl = t.variableDeclaration('const', [
+						t.variableDeclarator(
+							selectedId,
+							t.memberExpression(
+								mapId,
+								localeId,
+								true
+							)
+						)
+					]);
+
+					const useEffectAsyncFetch = t.expressionStatement(
+						t.callExpression(t.memberExpression(reactImportAlias, t.identifier('useEffect')), [
+							t.arrowFunctionExpression(
+								[],
+								t.blockStatement([
+									t.expressionStatement(
+										t.callExpression(
+											t.memberExpression(
+												t.callExpression(selectedId, []),
+												t.identifier('then')
+											),
+											[t.arrowFunctionExpression(
+												[t.identifier('f')],
+												t.callExpression(
+													setResultId,
+													[t.callExpression(
+														t.identifier('f'),
+														[propsId]
+													)]
+												)
+											)]
+										)
+									)
+								])
+							),
+							t.arrayExpression([t.identifier('locale')])
+						])
+					)
+
+					const returnStmt = t.returnStatement(resultId);
+
+					const hookId = t.identifier(hookName)
+					const functionDecl = t.functionDeclaration(
+						hookId,
+						[propsId],
+						t.blockStatement([
+							mapDecl,
+							useLocaleDecl,
+							usePrevLocaleDecl,
+							useStateResultDecl,
+							useEffectUpdatePrevDecl,
+							selectedDecl,
+							useEffectAsyncFetch,
+							returnStmt
+						])
+					)
+
+					ast.program.body.unshift(functionDecl)
+					ast.program.body.unshift(...asyncHooksDecls)
+					ast.program.body.unshift(defaultHookDecl)
+
+					nodePath.replaceWith(
+						t.callExpression(
+							hookId,
+							argAttr != undefined ? [argAttr] : []
+						)
+					);
+					modified = true;
+				}
 			});
 
 			translations.forEach((map, locale) => {
 				const exports: string[] = [];
 				map.forEach((expr, key) => {
-					const jsxCode = generate(expr, {}).code;
+					const jsCode = generate(expr, {}).code;
 					if (t.isArrowFunctionExpression(expr) || t.isFunctionExpression(expr)) {
-						// Se l'utente ha scritto ({ name }) => <>Ciao, {name}</>
-						// lo esportiamo così com'è, come componente React
-						exports.push(`export const ${key} = ${jsxCode};`);
+						exports.push(`export const ${key} = ${jsCode};`);
 					} else {
-						// altrimenti incapsuliamo il literal/JSX in un componente che non prende props
-						exports.push(`export const ${key} = ({}) => (${jsxCode});`);
+						exports.push(`export const ${key} = () => (${jsCode});`);
 					}
 				});
 
@@ -321,51 +544,16 @@ export default { ${Array.from(map.keys()).join(', ')} };
 			});
 
 			if (modified) {
-				let hasReactDefaultImport = false;
-
-				ast.program.body.forEach(node => {
-					if (
-						t.isImportDeclaration(node) &&
-						node.specifiers.some(spec =>
-							t.isImportDefaultSpecifier(spec) && spec.local.name === "React"
-						)
-					) {
-						hasReactDefaultImport = true;
-					}
-				});
-
-				if (!hasReactDefaultImport) {
-					// Add 'import React from 'react';
-					ast.program.body.unshift(
-						t.importDeclaration(
-							[t.importDefaultSpecifier(t.identifier("React"))],
-							t.stringLiteral("react")
-						)
-					);
-				}
-
-				let hasUseLocaleImport = false;
-
-				ast.program.body.forEach(node => {
-					if (
-						t.isImportDeclaration(node) &&
-						node.specifiers.some(spec =>
-							t.isImportSpecifier(spec) && spec.local.name === useLocaleName
-						)
-					) {
-						hasUseLocaleImport = true;
-					}
-				});
-
-				if (!hasUseLocaleImport) {
-					// Add 'import { useLocaleName } from 'useLocaleImportPath';
-					ast.program.body.unshift(
-						t.importDeclaration(
-							[t.importSpecifier(t.identifier(useLocaleName), t.identifier(useLocaleName))],
-							t.stringLiteral(useLocaleImportPath)
-						)
-					);
-				}
+				ast.program.body.unshift(
+					t.importDeclaration(
+						[t.importDefaultSpecifier(reactImportAlias)],
+						t.stringLiteral("react")
+					),
+					t.importDeclaration(
+						[t.importSpecifier(useLocaleImportAlias, t.identifier(useLocaleName))],
+						t.stringLiteral(useLocaleImportPath)
+					)
+				);
 
 				const output = generate(ast, {}, code).code;
 
@@ -378,7 +566,8 @@ export default { ${Array.from(map.keys()).join(', ')} };
 			return null;
 		},
 
-		async handleHotUpdate({ file, server, modules }) {
+		// 4) Handle Hot Module Reload
+		async handleHotUpdate({ file, server }) {
 			if (!file.endsWith('.tsx') || !filter(file)) {
 				return;
 			}
--- helper.tsx
+++ helper.tsx
@@ -1,42 +1,143 @@
 import React from 'react';
 import { useLocale, type LocaleType } from YOUR_LOCALE_PACKAGE;
 
-type TranslationMap<P extends object, R> = {
-	[L in LocaleType]: R | ((props: P) => R);
+const DEFAULT_STATIC_LOCALE: LocaleType = YOUR_DEFAULT_STATIC_LOCALE;
+
+type TranslationMap<T> = {
+	[L in LocaleType]: T;
 };
 
-export function T<P extends object = {}>(props: TranslationMap<P, React.ReactNode> & P): React.ReactNode {
+export function T<P extends object>(props: TranslationMap<React.ReactNode | ((props: P) => React.ReactNode)> & P): React.ReactNode {
 	const locale = useLocale();
 	const selected = props[locale];
 
-	// se è funzione, la invochiamo con rest (di tipo P)
 	if (typeof selected === 'function') {
-		// TS non sa se è ReactNode o funzione, quindi castiamo
-		return (selected as (props: P) => React.ReactNode)(props);
+		return (selected)(props as P);
 	}
 	return selected;
 }
 
-export function useT<R, P extends object = {}>(locales: TranslationMap<P, R>, args = {} as P): R {
+/*
+
+	// File.tsx
+
+import { useLocale as AutoLocale_useLocale } from USE_LOCALE_PATH;
+import AutoLocale_React from "react";
+
+const AutoLocale_file__index_lang0 = AutoLocale_React.lazy(() => import("virtual:auto-locale/lang0.jsx").then(module => ({
+	default: module["file__index"]
+})));
+const AutoLocale_file__index_lang1 = AutoLocale_React.lazy(() => import("virtual:auto-locale/lang1.jsx").then(module => ({
+	default: module["file__index"]
+})));
+
+function AutoLocale_file__index(props) {
+	const Map = {
+		lang0: AutoLocale_file__index_lang0,
+		lang1: AutoLocale_file__index_lang1,
+		...
+  	};
+
+	const locale = AutoLocale_useLocale();
+	const prevLocaleRef = AutoLocale_React.useRef(locale);
+
+	AutoLocale_React.useEffect(() => {
+		prevLocaleRef.current = locale;
+	}, [locale]);
+
+	const Selected = Map[locale];
+	const Fallback = Map[prevLocaleRef.current];
+
+	return <AutoLocale_React.Suspense fallback={<Fallback {...props} />}><Selected {...props} /></AutoLocale_React.Suspense>;
+}
+
+	// virtual:auto-locale/lang.tsx
+
+export const file0__0 = () => (STATIC_JSX_ELEMENT_FROM_FILE);
+
+export const file1__1 = JSX_FUNCTION_COMPONENT_FROM_FILE;
+
+export default { file0__0, file1__1 };
+
+*/
+
+export function useT<T>(
+	locales: { [L in LocaleType]: T }
+): T;
+export function useT<T, R>(
+	locales: TranslationMap<R | ((props: T) => R)>,
+	args: T
+): R;
+export function useT(locales: TranslationMap<any>, args?: any): any {
 	const locale = useLocale();
 	const selected = locales[locale];
 
-	// se è funzione, la invochiamo con rest (di tipo P)
 	if (typeof selected === 'function') {
-		// TS non sa se è ReactNode o funzione, quindi castiamo
-		return (selected as (props: P) => R)(args);
+		return selected(args)
 	}
 	return selected;
 }
 
-export function t<R, P extends object = {}>(locales: TranslationMap<P, R>, args = {} as P): R {
-	const locale = useLocale();
-	const selected = locales[locale];
+/*
 
-	// se è funzione, la invochiamo con rest (di tipo P)
+	// File.tsx
+
+import { useLocale as AutoLocale_useLocale } from USE_LOCALE_PATH;
+import AutoLocale_React from "react";
+
+// if lang0 is default
+import { file__index as useAutoLocale_file__index_default } from "virtual:auto-locale/default.jsx";
+const useAutoLocale_file__index_lang0 = () => Promise.resolve(useAutoLocale_file__index_default);
+// if lang1 is not default
+const useAutoLocale_file__index_lang1 = () => import("virtual:auto-locale/lang1.jsx").then(module => module["file__index"]);
+
+function useAutoLocale_file__index(props) {
+	const map = {
+		lang0: useAutoLocale_file__index_lang0,
+		lang1: useAutoLocale_file__index_lang1,
+		...
+	};
+
+	const locale = AutoLocale_useLocale();
+	const prevLocaleRef = AutoLocale_React.useRef(locale);
+	const [result, setResult] = AutoLocale_React.useState(useAutoLocale_file__index_default(props));
+
+	AutoLocale_React.useEffect(() => {
+		prevLocaleRef.current = locale;
+	}, [locale]);
+
+	const selected = map[locale];
+	AutoLocale_React.useEffect(() => {
+		selected().then(f => setResult(f(props)))
+	}, [locale]);
+
+	return result;
+}
+
+	// virtual:auto-locale/lang.tsx
+
+export const file0__0 = () => (STATIC_JSX_ELEMENT_FROM_FILE);
+
+export const file1__1 = JSX_FUNCTION_COMPONENT_FROM_FILE;
+
+export default { file0__0, file1__1 };
+
+*/
+
+export function t<T>(
+	locales: { [L in LocaleType]: T }
+): T;
+
+export function t<T, R>(
+	locales: TranslationMap<(props: T) => R>,
+	args: T
+): R;
+
+export function t(locales: TranslationMap<any>, args?: any): any {
+	const selected = locales[DEFAULT_STATIC_LOCALE];
+
 	if (typeof selected === 'function') {
-		// TS non sa se è ReactNode o funzione, quindi castiamo
-		return (selected as (props: P) => R)(args);
+		return selected(args)
 	}
 	return selected;
 }
