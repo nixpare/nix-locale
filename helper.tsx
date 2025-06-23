d4df4243aa3596e33620937907869a5fb1cc267a
Alessio Pareto (MacBook Pro di Pare) <alessio.pareto@nixpare.com>
2025-06-23 12:44:45 +0200

    Added new helpers to leverage future featrures

--- helper.tsx
+++ helper.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useLocale, type LocaleType } from YOUR_LOCALE_PACKAGE;
 
-type TranslationMap<P extends object> = {
-	[L in LocaleType]: React.ReactNode | ((props: P) => React.ReactNode);
+type TranslationMap<P extends object, R> = {
+	[L in LocaleType]: R | ((props: P) => R);
 };
 
-export function T<P extends object = {}>(props: TranslationMap<P> & P) {
+export function T<P extends object = {}>(props: TranslationMap<P, React.ReactNode> & P): React.ReactNode {
 	const locale = useLocale();
 	const selected = props[locale];
 
@@ -13,6 +13,30 @@ export function T<P extends object = {}>(props: TranslationMap<P> & P) {
 	if (typeof selected === 'function') {
 		// TS non sa se è ReactNode o funzione, quindi castiamo
 		return (selected as (props: P) => React.ReactNode)(props);
+	}
+	return selected;
+}
+
+export function useT<R, P extends object = {}>(locales: TranslationMap<P, R>, args = {} as P): R {
+	const locale = useLocale();
+	const selected = locales[locale];
+
+	// se è funzione, la invochiamo con rest (di tipo P)
+	if (typeof selected === 'function') {
+		// TS non sa se è ReactNode o funzione, quindi castiamo
+		return (selected as (props: P) => R)(args);
+	}
+	return selected;
+}
+
+export function t<R, P extends object = {}>(locales: TranslationMap<P, R>, args = {} as P): R {
+	const locale = useLocale();
+	const selected = locales[locale];
+
+	// se è funzione, la invochiamo con rest (di tipo P)
+	if (typeof selected === 'function') {
+		// TS non sa se è ReactNode o funzione, quindi castiamo
+		return (selected as (props: P) => R)(args);
 	}
 	return selected;
 }
